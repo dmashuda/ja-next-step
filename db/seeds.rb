4fdc7c44b11a1e4489409be67cae84412e342b59
Dan Mashuda <dmashuda@ycp.edu>
2016-06-21 20:29:12 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+easy_email = "abc123"
+
+user = User.new
+user.email = "dmashuda@ycp.edu"
+user.password = easy_email
+user.password_confirmation = easy_email
+user.save!
