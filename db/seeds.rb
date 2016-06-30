ec6698a94f8e5ca21ef4aa0c797f7ff4bbb218aa
Dan Mashuda <dmashuda@ycp.edu>
2016-06-30 00:23:57 -0400

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,28 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-easy_email = "abc123"
+easy_pass = "abc123"
 
-user = User.new
-user.email = "dmashuda@ycp.edu"
-user.password = easy_email
-user.password_confirmation = easy_email
-user.save!
+kw_ruby = Keyword.create(label: 'ruby')
+kw_rails = Keyword.create(label: 'rails')
+kw_go = Keyword.create(label: 'golang')
+kw_java = Keyword.create(label: 'java')
 
 
-swe = CareerField.create({label: 'software engineering'})
-swe.keywords.create(label: 'ruby')
-swe.keywords.create(label: 'rails')
-swe.keywords.create(label: 'golang')
-swe.keywords.create(label: 'java')
+kw_writing = Keyword.create(label: 'writing')
+kw_journalism = Keyword.create(label: 'journalism')
+kw_autocad = Keyword.create(label: 'autocad')
+
+cf_swe = CareerField.create({active: true, label: 'software engineering', keywords: [kw_go, kw_java, kw_ruby, kw_rails]})
+cf_me = CareerField.create({active: false, label: 'mechanical engineering', keywords: [kw_autocad]})
+
+
+sh_df = Stakeholder.create(name: 'dataforma', keywords: [kw_java])
+sh_df.users.create(email: 'dmashuda@ycp.edu', password: easy_pass, password_confirmation: easy_pass)
+
+df_profile = sh_df.build_profile
+df_profile.name = 'Dataforma Inc'
+df_profile.website_url = 'www.dataforma.com'
+df_profile.save
+
 
