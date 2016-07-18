162c6d8e7eae901f88e8e4b86d0c2c09004de517
vmoley <vmoley@dataforma.com>
2016-07-17 20:47:10 -0400

    added required fields

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ActiveRecord::Base
   belongs_to :stakeholder
   has_attached_file :photo
 
+  validates :subject, :abstract, :body, :contact_email, :email_subject, :email_body, :active, :stakeholder_id, :presence => true
+
   validates_attachment :photo,
                        content_type: { content_type: ['image/jpeg', 'image/gif', 'image/png'] }
 end
