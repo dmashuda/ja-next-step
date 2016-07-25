4d0c2ccac8f12ab6f27a7c2c699d1b5d6c8ab4f2
vmoley <vmoley@dataforma.com>
2016-07-25 12:33:02 -0400

    changed homepage to show posts

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'career_fields#index'
+  root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
