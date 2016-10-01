c9f5abdf6493d943e793fdcfbba662162dbd198e
mgaydash <mgaydash@dataforma.com>
2016-10-01 08:02:12 -0400

    Update paperclip env variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,9 +85,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
       :storage => :s3,
       :s3_credentials => {
-          :bucket => ENV['S3_BUCKET_NAME'],
-          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+          :bucket => ENV['PAPERCLIP_BUCKET_NAME'],
+          :access_key_id => ENV['PAPERCLIP_ACCESS_KEY_ID'],
+          :secret_access_key => ENV['PAPERCLIP_SECRET_ACCESS_KEY']
       }
   }
 end
