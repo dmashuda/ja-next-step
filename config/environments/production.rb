fed9696ae69ef444296cdd60de8cb84730b72dc7
mgaydash <mgaydash@dataforma.com>
2016-10-22 17:19:12 -0400

    Setup actionmailer with aws ses

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,9 +77,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {
-      host: 'localhost',
-      port: 3000
+  config.action_mailer.delivery_method = :aws_sdk
+
+  config.action_mailer.default_options = {
+      from: "noreply@janextstep.org"
   }
 
   config.paperclip_defaults = {
